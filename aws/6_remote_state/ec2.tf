94a288a29405e1792bdf9afa1543669834f35c80
itanmoyezio <37540476+itanmoyezio@users.noreply.github.com>
2020-09-24 16:48:26 +0530

    Update ec2.tf

--- aws/6_remote_state/ec2.tf
+++ aws/6_remote_state/ec2.tf
@@ -1,9 +1,9 @@
-resource "aws_instance" "myawsserver" {
+resource "aws_instance" "Tanmoyawsserver" {
   ami = "ami-0603cbe34fd08cb81"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "Techlanders-aws-ec2-instance"
+    Name = "Tanmoy-aws-ec2-instance"
     Env = "Prod"
   }
 }
