567b635bc8ce4c8ea6ae4e272d446a53ac50dcd9
itanmoyezio <37540476+itanmoyezio@users.noreply.github.com>
2020-09-24 17:30:07 +0530

    Update remote-state.tf

--- aws/6_remote_state/remote-state.tf
+++ aws/6_remote_state/remote-state.tf
@@ -1,6 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "terraformtanmoy"
+    key = "abc"
     region = "us-east-1"
 #   access_key = "XXXXXXXXXXXXXXXXXXXXXX"
 #   secret_key = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
